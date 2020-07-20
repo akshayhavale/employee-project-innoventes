5ab5b4a73bde12712bbcab018cfdd6547ed70ae4
Akshay Havale <akshayhavale22@gmail.com>
2020-07-20 13:24:58 +0530

    Initial project setup

---
+++ employee-project-innoventes/src/main/java/com/employee/EmployeeProjectInnoventesApplication.java
@@ -0,0 +1,13 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProjectInnoventesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProjectInnoventesApplication.class, args);
+	}
+
+}
